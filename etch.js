09c99ef79a50d3c36f7aad27b1af8b3945f09485
SlipStreme <Kinloes@gmail.com>
2023-07-24 20:05:37 -0400

    Changed the divs to work in the DOM instead of through html.

--- etch.js
+++ etch.js
@@ -1,6 +1,15 @@
+//This creates the grid
+let makeGrid = document.createElement("div");
+makeGrid.id = "makeGrid";
+let makeGrid2 = sketchContainer.appendChild(makeGrid);
 const etch = document.getElementById("sketchContainer");
-const div = document.getElementById("1")
-div.addEventListener("click", placeSquare);
+etch.addEventListener("click", placeSquare);
+
+for (i = 1; i <= 256; i++){
+    let nextDiv = document.createElement("div");
+    nextDiv.id = "clickemDiv" + i;
+    sketchContainer.appendChild(nextDiv);
+    }
 function placeSquare(){
-    div.style.backgroundColor = "blue";
+    
 }

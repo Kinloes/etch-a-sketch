10d2f538b0fd31a717b66ae22e18b7c8ba9ca8d7
SlipStreme <Kinloes@gmail.com>
2023-07-25 18:40:03 -0400

    The page now properly olors in the individual squares when clicking

--- etch.js
+++ etch.js
@@ -1,15 +1,11 @@
 //This creates the grid
-let makeGrid = document.createElement("div");
-makeGrid.id = "makeGrid";
-let makeGrid2 = sketchContainer.appendChild(makeGrid);
-const etch = document.getElementById("sketchContainer");
-etch.addEventListener("click", placeSquare);
-
 for (i = 1; i <= 256; i++){
-    let nextDiv = document.createElement("div");
-    nextDiv.id = "clickemDiv" + i;
-    sketchContainer.appendChild(nextDiv);
+    let makeGrid = document.createElement("div");
+    makeGrid.id = "makeGrid" + i;
+    sketchContainer.appendChild(makeGrid);
+//This adds the click element
+    makeGrid.addEventListener("click", placeSquare);
+    function placeSquare(){
+        makeGrid.style.backgroundColor = "black";
     }
-function placeSquare(){
-    
 }

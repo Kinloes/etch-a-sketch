844834bc18f3d734b25f3acd8141008ac0be9c5f
SlipStreme <Kinloes@gmail.com>
2023-07-26 16:37:08 -0400

    Added buttons Testing possible functionalities

--- etch.js
+++ etch.js
@@ -1,21 +1,31 @@
+//This allows the "click and drag" drawing functionality
 document.addEventListener("mousedown", function(){
     trigger = true
 })
 document.addEventListener("mouseup", function(){
     trigger = false
 })
+//eventlisteners for buttons
+test.addEventListener("click", function(){
+    test.style.boxShadow = "inset 0px 0px 4px";
+})
+erase.addEventListener("click", clickButton)
+    function clickButton(){
+    erase.style.boxShadow = "inset 0px 0px 4px";
+    eraseTrigger = true;
+    }
 //This creates the grid
 for (i = 1; i <= 256; i++){
     let makeGrid = document.createElement("div");
     makeGrid.id = "makeGrid" + i;
     sketchContainer.appendChild(makeGrid);
 //This adds the click element
-    makeGrid.addEventListener("mousedown", placeSquare2)
-    function placeSquare2(){
+    makeGrid.addEventListener("mousedown", placeSquare)
+    function placeSquare(){
         makeGrid.style.backgroundColor = "black"
     }
-    makeGrid.addEventListener("mouseover", placeSquare)
-    function placeSquare(){
+    makeGrid.addEventListener("mouseover", placeSquare2)
+    function placeSquare2(){
         if (trigger === true){
             makeGrid.style.backgroundColor = "black";
         }

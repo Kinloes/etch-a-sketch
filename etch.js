3618af190aeee4ad95a7dd374b975925d49fa5b9
SlipStreme <Kinloes@gmail.com>
2023-07-26 15:32:49 -0400

    Program now reacts accordingly to initial click. Program is now fully functional at a base level.

--- etch.js
+++ etch.js
@@ -1,11 +1,23 @@
+document.addEventListener("mousedown", function(){
+    trigger = true
+})
+document.addEventListener("mouseup", function(){
+    trigger = false
+})
 //This creates the grid
 for (i = 1; i <= 256; i++){
     let makeGrid = document.createElement("div");
     makeGrid.id = "makeGrid" + i;
     sketchContainer.appendChild(makeGrid);
 //This adds the click element
-    makeGrid.addEventListener("click", placeSquare);
+    makeGrid.addEventListener("mousedown", placeSquare2)
+    function placeSquare2(){
+        makeGrid.style.backgroundColor = "black"
+    }
+    makeGrid.addEventListener("mouseover", placeSquare)
     function placeSquare(){
-        makeGrid.style.backgroundColor = "black";
+        if (trigger === true){
+            makeGrid.style.backgroundColor = "black";
+        }
     }
 }

cd73af3820830f663617df457af4c1cb2d62293d
SlipStreme <Kinloes@gmail.com>
2023-07-26 21:41:35 -0400

    Added drawing and erasing functionality Each only works when button pressed Button functionality currently overlaps

--- etch.js
+++ etch.js
@@ -1,3 +1,6 @@
+drawButton = false;
+eraseButton = false;
+
 //This allows the "click and drag" drawing functionality
 document.addEventListener("mousedown", function(){
     trigger = true
@@ -5,29 +8,42 @@ document.addEventListener("mousedown", function(){
 document.addEventListener("mouseup", function(){
     trigger = false
 })
+
 //eventlisteners for buttons
-test.addEventListener("click", function(){
-    test.style.boxShadow = "inset 0px 0px 4px";
-})
-erase.addEventListener("click", clickButton)
-    function clickButton(){
+document.getElementById("draw").onclick = function(){
+    draw.style.boxShadow = "inset 0px 0px 4px";
+    drawButton = true;
+}
+document.getElementById("erase").onclick = function(){
     erase.style.boxShadow = "inset 0px 0px 4px";
-    eraseTrigger = true;
-    }
+    eraseButton = true;
+}
+
 //This creates the grid
 for (i = 1; i <= 256; i++){
     let makeGrid = document.createElement("div");
     makeGrid.id = "makeGrid" + i;
     sketchContainer.appendChild(makeGrid);
+
 //This adds the click element
     makeGrid.addEventListener("mousedown", placeSquare)
     function placeSquare(){
+        if (drawButton === true){
         makeGrid.style.backgroundColor = "black"
+        }
+        if (eraseButton === true){
+            makeGrid.style.backgroundColor = "white"
+        }
     }
     makeGrid.addEventListener("mouseover", placeSquare2)
     function placeSquare2(){
         if (trigger === true){
-            makeGrid.style.backgroundColor = "black";
+            if (drawButton === true){
+                makeGrid.style.backgroundColor = "black"
+                }
+            if (eraseButton === true){
+                makeGrid.style.backgroundColor = "white"
+            }
         }
     }
 }

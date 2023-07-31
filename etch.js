f80a8c7c71dc12732ff9bc9b72c15b7f86fe6980
SlipStreme <Kinloes@gmail.com>
2023-07-30 23:26:44 -0400

    Added a color selector

--- etch.js
+++ etch.js
@@ -1,6 +1,7 @@
 drawButton = false;
 eraseButton = false;
 rainbowButton = false;
+playerColor = false;
 //Random number generator for rainbow button
 function randomnum(min, max){
     return Math.floor(Math.random() * 255) + 1;
@@ -39,6 +40,14 @@ document.getElementById("rainbow").onclick = function(){
         rainbowButtonTru();
     }
 }
+colorPicker.addEventListener("input", updateFirst, false);
+function updateFirst(){
+    playerColor = true;
+    if (playerColor = true){
+        colorPicker.style.boxShadow = "inset 0px 0px 4px";
+        playerColorTru();
+    }
+}
 
 //Removes the "button press" CSS and functionality for non-active buttons
 function drawButtonTru(){
@@ -46,18 +55,32 @@ function drawButtonTru(){
     eraseButton = false;
     rainbow.style.boxShadow = "inset 0px 0px 0px";
     rainbowButton = false;
+    colorPicker.style.boxShadow = "inset 0px 0px 0px";
+    playerColor = false;
 }
 function eraseButtonTru(){
     draw.style.boxShadow = "inset 0px 0px 0px";
     drawButton = false;
     rainbow.style.boxShadow = "inset 0px 0px 0px";
     rainbowButton = false;
+    colorPicker.style.boxShadow = "inset 0px 0px 0px";
+    playerColor = false;
 }
 function rainbowButtonTru(){
     erase.style.boxShadow = "inset 0px 0px 0px";
     eraseButton = false;
     draw.style.boxShadow = "inset 0px 0px 0px";
     drawButton = false;
+    colorPicker.style.boxShadow = "inset 0px 0px 0px";
+    playerColor = false;
+}
+function playerColorTru(){
+    draw.style.boxShadow = "inset 0px 0px 0px";
+    drawButton = false;
+    erase.style.boxShadow = "inset 0px 0px 0px";
+    eraseButton = false;
+    rainbow.style.boxShadow = "inset 0px 0px 0px";
+    rainbowButton = false;
 }
 //This creates the grid
 for (i = 1; i <= 256; i++){
@@ -77,6 +100,9 @@ for (i = 1; i <= 256; i++){
         if (rainbowButton === true){
             makeGrid.style.backgroundColor = "rgb(" + randomnum() + "," + randomnum() + "," + randomnum() + ")";
         }
+        if (playerColor === true){
+            makeGrid.style.backgroundColor = colorPicker.value
+        }
     }
 //This adds a "drag" functionality that continually adds colored squares as the user holds the button
     makeGrid.addEventListener("mouseover", placeSquare2)
@@ -90,6 +116,9 @@ for (i = 1; i <= 256; i++){
                 }
             if (rainbowButton === true){
                 makeGrid.style.backgroundColor = "rgb(" + randomnum() + "," + randomnum() + "," + randomnum() + ")";
+                }
+                if (playerColor === true){
+                    makeGrid.style.backgroundColor = colorPicker.value
                 }
         }
     }
